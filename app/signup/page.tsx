2ac3c0dcb3ccfc558ef546242f1753ddf643531e
eusebiucroitor <119120965+eusebiucroitor@users.noreply.github.com>
2024-02-27 22:10:21 +0200

    Added-employee-signup

--- app/signup/page.tsx
+++ app/signup/page.tsx
@@ -10,85 +10,84 @@ import InputField from "@/components/InputField";
 import { useRouter } from "next/navigation";
 
 export const formSchema = z.object({
-    name: z.string().min(2, {
-        message: "Username must be at least 2 characters.",
-    }),
-    email: z.string().email(),
-    password: z.string().min(8),
-    organizationName: z.string().min(3),
-    headquarterAddress: z.string().min(3),
+  name: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+  email: z.string().email(),
+  password: z.string().min(8),
+  organizationName: z.string().min(3),
+  headquarterAddress: z.string().min(3),
 });
 
-
 const SignupPage = () => {
-    const router = useRouter();
+  const router = useRouter();
 
-    const form = useForm<z.infer<typeof formSchema>>({
-        resolver: zodResolver(formSchema),
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+  });
+
+  async function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(process.env.SIGNUP_ADMIN_URL);
+
+    const res = await fetch(process.env.NEXT_PUBLIC_SIGNUP_ADMIN_URL!, {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(values),
     });
 
-    async function onSubmit(values: z.infer<typeof formSchema>) {
-        console.log(process.env.SIGNUP_ADMIN_URL);
-    
-        const res = await fetch(process.env.NEXT_PUBLIC_SIGNUP_ADMIN_URL!, {
-            method: "POST",
-            headers: { "Content-Type": "application/json" },
-            body: JSON.stringify(values),
-        });
-    
-        if (!res.ok) return null;
+    if (!res.ok) return null;
 
-        router.push('/api/auth/signin');
-    }
+    router.push("/api/auth/signin");
+  }
 
-    return (
-        <div className="flex">
-            <div className="bg-slate-300 h-screen flex-1"></div>
-            <Form {...form}>
-                <form
-                    onSubmit={form.handleSubmit(onSubmit)}
-                    className="space-y-2 flex-1 p-8"
-                >
-                    <InputField
-                        name="name"
-                        label="Name"
-                        placeholder="Name"
-                        type="text"
-                        control={form.control}
-                    />
-                    <InputField
-                        name="email"
-                        label="Email"
-                        placeholder="Email"
-                        type="email"
-                        control={form.control}
-                    />
-                    <InputField
-                        name="password"
-                        label="Password"
-                        placeholder="Password"
-                        type="password"
-                        control={form.control}
-                    />
-                    <InputField
-                        name="organizationName"
-                        label="Organization"
-                        placeholder="Organization"
-                        type="text"
-                        control={form.control}
-                    />
-                    <InputField
-                        name="headquarterAddress"
-                        label="Address"
-                        placeholder="Address"
-                        type="text"
-                        control={form.control}
-                    />
-                    <Button type="submit">Submit</Button>
-                </form>
-            </Form>
-        </div>
-    );
+  return (
+    <div className="flex">
+      <div className="bg-slate-300 h-screen flex-1"></div>
+      <Form {...form}>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-2 flex-1 p-8"
+        >
+          <InputField
+            name="name"
+            label="Name"
+            placeholder="Name"
+            type="text"
+            control={form.control}
+          />
+          <InputField
+            name="email"
+            label="Email"
+            placeholder="Email"
+            type="email"
+            control={form.control}
+          />
+          <InputField
+            name="password"
+            label="Password"
+            placeholder="Password"
+            type="password"
+            control={form.control}
+          />
+          <InputField
+            name="organizationName"
+            label="Organization"
+            placeholder="Organization"
+            type="text"
+            control={form.control}
+          />
+          <InputField
+            name="headquarterAddress"
+            label="Address"
+            placeholder="Address"
+            type="text"
+            control={form.control}
+          />
+          <Button type="submit">Submit</Button>
+        </form>
+      </Form>
+    </div>
+  );
 };
 
 export default SignupPage;
---
+++ components/SignUpEmployee.tsx
@@ -0,0 +1,47 @@
+import { UseFormReturn } from "react-hook-form";
+import React from "react";
+import { z } from "zod";
+
+import {
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { formSchemaSignUp } from "@/app/[orgId]/signup/page";
+
+interface Props {
+  control: UseFormReturn<z.infer<typeof formSchemaSignUp>>["control"];
+  name: keyof z.infer<typeof formSchemaSignUp>;
+  label: string;
+  placeholder: string;
+  type: string;
+}
+
+const InputField: React.FC<Props> = ({
+  name,
+  label,
+  placeholder,
+  type,
+  control,
+}: Props) => {
+  return (
+    <FormField
+      control={control}
+      name={name}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{label}</FormLabel>
+          <FormControl>
+            <Input placeholder={placeholder} type={type} {...field} />
+          </FormControl>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+export default InputField;
