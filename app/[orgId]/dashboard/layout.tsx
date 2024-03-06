810225fa255d4dcc6ca2e514ec9611498f5978bf
Croitor Eusebiu <119120965+eusebiucroitor@users.noreply.github.com>
2024-03-06 11:46:00 +0200

    Enhancing-ui-design-sidebar (#19)

--- app/[orgId]/dashboard/layout.tsx
+++ app/[orgId]/dashboard/layout.tsx
@@ -2,11 +2,9 @@ import Sidebar from "@/components/Sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-screen flex flex-row justify-start ">
+    <div className="h-screen flex flex-row justify-start w-240 ">
       <Sidebar />
-      <div className="flex-1 p-2">
-        {children}
-      </div>
+      <div className="flex-1 p-2 ">{children}</div>
     </div>
   );
 }
