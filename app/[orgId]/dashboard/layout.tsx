cd64379cd668ef461a40cf2c2c5298b6aadb71ef
eusebiucroitor <119120965+eusebiucroitor@users.noreply.github.com>
2024-03-09 15:41:31 +0200

    Create responsive sidbar

--- app/[orgId]/dashboard/layout.tsx
+++ app/[orgId]/dashboard/layout.tsx
@@ -1,14 +1,82 @@
+"use client";
+import React, { useEffect, useState } from "react";
 import Sidebar from "@/components/Sidebar";
+import { CiMenuBurger } from "react-icons/ci";
 
-export default function Layout({ children }: { children: React.ReactNode }) {
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+  const [isMobile, setIsMobile] = useState(false);
+  const [showMenuItems, setShowMenuItems] = useState(false);
+
+  const toggleSidebar = () => {
+    setIsSidebarOpen(!isSidebarOpen);
+  };
+
+  const handleClick = () => {
+    if (!isMobile) {
+    }
+  };
+
+  useEffect(() => {
+    const handleResize = () => {
+      const newIsMobile = window.innerWidth <= 780; // Adjust the width as needed
+      setIsMobile(newIsMobile);
+    };
+
+    // Initial check on mount
+    handleResize();
+
+    // Add event listener for window resize
+    window.addEventListener("resize", handleResize);
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  const sidebarStyle = isMobile ? "ml-[0] w-[100%]" : "ml-[240px]";
+
   return (
-    <div className="flex min-h-screen bg-gray-100 ">
-      <Sidebar />
-      <div className="ml-[240px] flex-1">
-        <div className=" m-2 min-h-screen rounded-lg  border-2 border-gray-300 bg-white p-4">
-          {children}
+    <div className="relative flex bg-gray-100">
+      <div className="relative flex-1 overflow-hidden">
+        <Sidebar
+          isOpen={isSidebarOpen}
+          toggleSidebar={toggleSidebar}
+          handleClick={handleClick}
+        />
+        <div className={`m-2 ${sidebarStyle}`}>
+          {/* Navbar */}
+          <div className={`p-4 ${isMobile ? "flex justify-between" : ""}`}>
+            {isMobile && (
+              <CiMenuBurger
+                className=":hidden h-6 w-6 cursor-pointer "
+                onClick={toggleSidebar}
+              />
+            )}
+          </div>
+          {/* Content */}
+          <div
+            className={`relative z-10 min-h-screen rounded-lg border-2 border-gray-300 bg-white p-4 ${
+              isSidebarOpen ? "ml-0" : "ml-[20px]"
+            } transition-all duration-300 ease-in-out`}
+            style={{
+              position: isSidebarOpen && isMobile ? "absolute" : "relative",
+              left: isSidebarOpen && isMobile ? "100%" : "0",
+            }}
+          >
+            {/* Pass the handleClick function to Sidebar */}
+
+            {children}
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
+export default Layout;
